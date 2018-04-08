5dd8a9ce8908d3c4eb0e14d3a85e11a7d472a6c3
ferdizz <ferdinandforgaard@gmail.com>
2018-04-08 15:03:14 +0200

    Added axios for HTTP requests

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,10 +1,20 @@
+import axios from 'axios'
 import * as types from './actionTypes'
 
 export const login = (dispatch, userdata) => {
-    dispatch({
-        type: types.LOG_IN,
-        data: userdata
-    })
+
+    axios.post('http://localhost:3001/users/login', userdata)
+        .then(response => {
+            console.log('Response from server:')
+            console.log(response)
+        }).catch(error => {
+            console.log(error)
+        })
+
+    // dispatch({
+    //     type: types.LOG_IN,
+    //     data: userdata
+    // })
 }
 
 export const createUser = (dispatch, userdata) => {
