1d948c7ec192440cf5f65cd68de9d0c6b450d56e
ferdizz <ferdinandforgaard@gmail.com>
2018-04-08 14:57:36 +0200

    Added jwt auth

--- server/api/controllers/user.js
+++ server/api/controllers/user.js
@@ -1,6 +1,8 @@
 const User = require('../models/user');
+const jwt = require('jsonwebtoken')
 
 exports.get_users = (req, res, next) => {
+  console.log(req.userData)
   User.find({}, '-__v').then((users) => {
     res.status(200).send(users);
   }).catch(next);
@@ -50,8 +52,16 @@ exports.login = (req, res, next) => {
       });
     }
 
+    const token = jwt.sign({
+      user_id: user._id,
+      username: user.username
+    }, process.env.JWT_KEY, {
+      expiresIn: "1h"
+    });
+
     res.status(200).json({
-      message: 'Login successful'
+      message: 'Login successful',
+      token: token
     });
 
   });
--- server/api/routes/user.js
+++ server/api/routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/check_auth')
 
 const UserController = require('../controllers/user');
 
-router.get('/', UserController.get_users);
+router.get('/', auth, UserController.get_users);
 router.get('/:id', UserController.get_user_by_id);
 
 router.post('/signup', UserController.signup);
