16e35fb3e88c7913e181bd9cda101f93f12930a5
Ferdinand Forgaard <ferdinand.forgaard@bouvet.no>
2018-10-12 13:38:23 +0200

    Set created user as active user

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,49 +1,58 @@
-import axios from 'axios'
-import * as types from './actionTypes'
-import { saveData } from '../utils/storage'
+import axios from 'axios';
+import * as types from './actionTypes';
+import { saveData } from '../utils/storage';
+
+const URL = 'http://localhost:3001/users';
 
 export const login = (dispatch, userdata) => {
-
-    axios.post('http://localhost:3001/users/login', userdata)
+    axios
+        .post(URL + '/login', userdata)
         .then(response => {
-            if (response.status === 200 && response.data.token) {
-                saveData('token', response.data.token)
-                saveData('user', userdata)
-                setUser(dispatch, userdata)
-                setStatus(dispatch, { status: response.data.message })
+            if (response.data.token) {
+                setUser(dispatch, userdata, response);
             }
         })
+        .catch(error => {
+            console.log(error.response);
+            setStatus(dispatch, { status: error.response.data.message });
+        });
+};
 
-        .catch((error, response) => {
-            console.log(error.response)
-            setStatus(dispatch, { status: error.response.data.message })
-        })
-}
+export const setUser = (dispatch, userdata, response) => {
+    saveData('user', userdata);
+    saveData('token', response.data.token);
+    setStatus(dispatch, { status: response.data.message });
 
-export const setUser = (dispatch, userdata) => {
     dispatch({
         type: types.LOG_IN,
         data: userdata
-    })
-}
+    });
+};
 
 export const createUser = (dispatch, userdata) => {
-    dispatch({
-        type: types.LOG_IN,
-        data: userdata
-    })
-}
+    axios
+        .post('http://localhost:3001/users/signup', userdata)
+        .then(response => {
+            if (response.data.token) {
+                setUser(dispatch, userdata, response);
+            }
+        })
+        .catch(error => {
+            console.log(error.response);
+            setStatus(dispatch, { status: error.response.data.message });
+        });
+};
 
 export const setStatus = (dispatch, status) => {
     dispatch({
         type: types.SET_STATUS,
         data: status
-    })
-}
+    });
+};
 
-export const logout = (dispatch) => {
+export const logout = dispatch => {
     localStorage.clear();
     dispatch({
         type: types.LOG_OUT
-    })
-}
+    });
+};
