7e047d11080de455fbfdbf64b5699a4a6858ac73
Ferdinand Forgaard <ferdinand.forgaard@bouvet.no>
2018-10-12 13:41:30 +0200

    Code formatting

--- server/api/routes/user.js
+++ server/api/routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../middleware/check_auth')
+const auth = require('../middleware/check_auth');
 
 const UserController = require('../controllers/user');
 
