14e1ae156c9c65ad3d493a035d4f712ff4e353f9
Ferdinand Forgaard <ferdinand.forgaard@bouvet.no>
2018-12-14 16:22:39 +0100

    Updated mealActions

--- client/src/actions/mealActions.js
+++ client/src/actions/mealActions.js
@@ -3,3 +3,5 @@ import { createAction } from 'redux-actions';
 import * as actions from './actionTypes';
 
 export const addMeal = createAction(actions.ADD_MEAL_REQUEST);
+export const addMealSuccess = createAction(actions.ADD_MEAL_SUCCESS);
+export const addMealFailure = createAction(actions.ADD_MEAL_FAILURE);
